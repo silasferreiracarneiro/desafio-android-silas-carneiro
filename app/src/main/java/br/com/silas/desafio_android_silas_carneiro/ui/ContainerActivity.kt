5dec69f96268202646e6f6c6c8ce284f51bcabfb
silasferreiracarneiro <silasferreira96@hotmail.com>
2020-06-20 16:27:24 -0300

    Configurando o navigation

--- app/src/main/java/br/com/silas/desafio_android_silas_carneiro/ui/ContainerActivity.kt
+++ app/src/main/java/br/com/silas/desafio_android_silas_carneiro/ui/ContainerActivity.kt
@@ -2,18 +2,17 @@ package br.com.silas.desafio_android_silas_carneiro.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import br.com.silas.desafio_android_silas_carneiro.R
-import br.com.silas.desafio_android_silas_carneiro.ui.characterList.CharacterListFragment
 
 class ContainerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.container_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CharacterListFragment.newInstance())
-                .commit()
-        }
+
+        val host = NavHostFragment.create(R.navigation.navigation)
+        supportFragmentManager.beginTransaction().replace(R.id.container, host)
+            .setPrimaryNavigationFragment(host).commit()
     }
 }
--- app/src/main/java/br/com/silas/desafio_android_silas_carneiro/ui/detailCharacter/DetailCharacterFragment.kt
+++ app/src/main/java/br/com/silas/desafio_android_silas_carneiro/ui/detailCharacter/DetailCharacterFragment.kt
@@ -1,4 +1,4 @@
-package br.com.silas.desafio_android_silas_carneiro.ui.characterList
+package br.com.silas.desafio_android_silas_carneiro.ui.detailCharacter
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import br.com.silas.desafio_android_silas_carneiro.R
 
-class CharacterListFragment : Fragment() {
+class DetailCharacterFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_character_list, container, false)
+        return inflater.inflate(R.layout.fragment_detail_character, container, false)
     }
 }
