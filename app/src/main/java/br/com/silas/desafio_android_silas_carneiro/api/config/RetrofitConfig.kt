e2beacdb2b27566ad1ba88148d70737a5a5de45e
silasferreiracarneiro <silasferreira96@hotmail.com>
2020-06-20 21:55:38 -0300

    ajustes no projeto

---
+++ app/src/main/java/br/com/silas/desafio_android_silas_carneiro/api/config/RetrofitConfig.kt
@@ -0,0 +1,4 @@
+package br.com.silas.desafio_android_silas_carneiro.api.config
+
+class RetrofitConfig {
+}
--- app/src/main/java/br/com/silas/desafio_android_silas_carneiro/repository/CharacterListRepository.kt
+++ app/src/main/java/br/com/silas/desafio_android_silas_carneiro/repository/CharacterListRepository.kt
@@ -1,4 +1,6 @@
 package br.com.silas.desafio_android_silas_carneiro.repository
 
-class CharacterListRepository {
+import br.com.silas.desafio_android_silas_carneiro.api.Api
+
+class CharacterListRepository(val api: Api) {
 }
